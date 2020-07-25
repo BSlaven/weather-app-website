8357297ffa890cc9029f0308d730df11dbfcb9e4
Slaven B <slaven.bunijevac@gmail.com>
2020-07-25 18:06:31 +0200

    inital commit

---
+++ src/app.js
@@ -0,0 +1,96 @@
+const path = require('path')
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Slaven B'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Slaven B'
+  })
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'Upomoooooć!',
+    title: 'Help',
+    name: 'Slaven B'
+  })
+});
+
+app.get('/weather', (req, res) => {
+  if(!req.query.address) {
+    return res.send({
+      error: 'You have to enter an address!'
+    });
+  }
+
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if(error) {
+      return res.send({ error });
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if(error) {
+        return res.send({ error })
+      }
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address
+      });
+    });
+  });
+});
+
+// app.get('/products', (req, res) => {
+//   if(!req.query.search) {
+//     return res.send({
+//       error: 'You must provide a search term'
+//     });
+//   }
+//   console.log(req.query);
+//   res.send({
+//     products: []
+//   });
+// });
+
+app.get('/help/*', (req, res) => {
+  res.render('404error', {
+    name: 'Slaven',
+    title: 'Error 404',
+    errorMessage: 'Help article not found!'
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404error', {
+    name: 'Slaven',
+    title: 'Error 404',
+    errorMessage: 'Page not found!'
+  });
+});
+
+
+app.listen(3000, () => {
+  console.log('Server je uspješno pokrenut na portu 3000!');
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+
+const forecast = (lat, long, callback) => {
+  const url = `https://api.darksky.net/forecast/5313af9d9a77df2b8b8cdbbc6a434d38/${lat},${long}?units=si`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if(error) {
+      callback('Unable to connect to weather services!', undefined);
+    } else if(body.error) {
+      callback('Unable to find location! Try again.', undefined);
+    } else {
+      callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees C outside with ${body.currently.precipProbability}% chance of rain.`);
+    }
+  })
+}
+
+
+module.exports = forecast;
