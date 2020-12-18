f8cf526aa82c736a9a012207bf02629e73e7fdfe
Slaven B <slaven.bunijevac@gmail.com>
2020-12-18 14:52:21 +0100

    add local language on the first message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 
 const forecast = (lat, long, callback) => {
-  const url = `https://api.darksky.net/forecast/5313af9d9a77df2b8b8cdbbc6a434d38/${lat},${long}?units=si`;
+  const url = `https://api.darksky.net/forecast/5313af9d9a77df2b8b8cdbbc6a434d38/${lat},${long}?units=si&lang=sr`;
 
   request({ url, json: true }, (error, { body }) => {
     if(error) {
@@ -10,9 +10,7 @@ const forecast = (lat, long, callback) => {
     } else if(body.error) {
       callback('Unable to find location! Try again.', undefined);
     } else {
-      callback(undefined, `${body.daily.data[0].summary}
-      <br>
-      Trenutno je ${body.currently.temperature} stepeni Celzijusa uz ${body.currently.precipProbability}% mogućnosti padavina.
+      callback(undefined, `${body.daily.data[0].summary} Trenutno je ${body.currently.temperature} stepeni Celzijusa uz ${body.currently.precipProbability}% mogućnosti padavina.
       Najniža temperatura danas je ${body.daily.data[0].temperatureLow}.
       Najviša temperatura danas je ${body.daily.data[0].temperatureHigh}.`);
     }
